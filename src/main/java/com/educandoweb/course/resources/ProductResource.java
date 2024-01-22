e4e47929bae87955cc4656cb53ca264dbf880498
Vinicius Muller <vinicius.muller20@outlook.com>
2024-01-22 05:12:18 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -13,7 +13,7 @@ import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.services.ProductService;
 
 @RestController
-@RequestMapping(value = "/producties")
+@RequestMapping(value = "/products")
 public class ProductResource {
 
 	@Autowired
